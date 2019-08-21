a3d81dacaf009b0964794e45bc9c9d320bf02d97
ashish-greycube <mr.ashish.shah@gmail.com>
2019-08-21 11:48:41 +0530

    removed frappe errprint

--- zuvi/api.py
+++ zuvi/api.py
@@ -5,11 +5,8 @@ import frappe
 
 @frappe.whitelist()
 def calculate_contribution(self,mehtod):
-    frappe.errprint('inside calculate_contribution api.py')
-    print('inside calculate_contribution api.py')
     if not self.meta.get_field("sales_team"):
         return
-
     total = 0.0
     sales_team = self.get("sales_team")
     # my_allocated_percentage=100/(len(sales_team))
@@ -20,6 +17,3 @@ def calculate_contribution(self,mehtod):
         sales_person.incentives=1
         sales_person.commission_rate=1
     return
-        # sales_person.allocated_amount = flt(
-        #     self.base_net_total * sales_person.allocated_percentage / 100.0,
-        #     self.precision("allocated_amount", sales_person))
